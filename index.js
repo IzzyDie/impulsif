c7d3c393a8221a98fa19983394b004d59c39d041
IzzyDie <47723602+IzzyDie@users.noreply.github.com>
2019-08-16 23:41:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-const White = require('./commands/white')
+const White = require('white')
 
 bot.on('ready', function () {
     bot.user.setAvatar('if.jpg')
